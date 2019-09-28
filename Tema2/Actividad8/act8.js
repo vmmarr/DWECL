8b5e1f973e32926cdae043e2a63edc117f510397
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2019-09-28 20:08:01 +0200

    Actividad 8

---
+++ Tema2/Actividad8/act8.js
@@ -0,0 +1,19 @@
+for (i = 0; i < 5; i++) {
+    switch (i) {
+        case 0:
+            alert('Uno');
+            break;
+        case 1:
+            alert('Dos');
+            break;
+        case 2:
+            alert('Tres');
+            break;
+        case 3:
+            alert('Cuatro');
+            break;
+        case 4:
+            alert('Cinco');
+            break;
+    }
+}
