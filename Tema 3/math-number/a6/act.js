6e369ff1389205e7cf020c2b369503f87f934996
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-10-02 19:27:10 +0200

    Actividades tema 3

---
+++ Tema 3/math-number/a6/act.js
@@ -0,0 +1,10 @@
+alert('Resultados que dan menos infinito');
+alert(`Math.max() + Math.max() = ${Math.max() + Math.max()}`);
+alert(`Math.min() * Math.max() = ${Math.min() * Math.max()}`);
+alert(`Math.max() / 0 = ${Math.max() / 0}`); 
+
+alert('Resultados que da infinito');
+alert(`Math.max() * Math.max() = ${Math.max() * Math.max()}`);
+alert(`Math.pow(Math.max(), 2) = ${Math.pow(Math.max(), 2)}`);
+alert(`5/0 = ${5/0}`);
+
