e259f244937a43e1abd1d482fb532de0404accc5
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2019-10-04 12:21:20 +0200

    Actividad 8

--- Tema 3/math-number/a8/act.js
+++ Tema 3/math-number/a8/act.js
@@ -1,4 +1,15 @@
+// Area del circulo
 var radio = prompt('Introduce el radio del circulo');
 var n = new Number(radio);
 
-alert(radio + 2);
+var ac = Math.PI * Math.pow(radio, 2);
+
+alert(`El area del circulo es ${ac.toFixed(2)} cm2`);
+
+// Area del triangulo
+
+var b = prompt('Introduce la base del triangulo');
+var h = prompt('Introduce la altura del triangulo');
+var at = (b * h) / 2;
+
+alert(`El area del triangulo es ${at.toFixed(2)} cm2`);
