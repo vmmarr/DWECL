031b25e4271eb6944fc0fe4f4c444537b83b0ea3
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-10-10 20:51:26 +0200

    Actividad 19 para revisar

--- Tema 3/regexp/a19/act.js
+++ Tema 3/regexp/a19/act.js
@@ -1,6 +1,7 @@
 var hora = prompt('Introduce una hra para validar el formato hh:mm:ss o hh:mm');
 
 var p = /^(\d{2})\:(\d{2})(\:(\d{2}))?$/;
+// var patron=/^(0[1-9]|1\d|2[0-3]):([0-5]\d)(:([0-5]\d))?$/;
 var pa = /^\:/;
 
 var r = p.exec(hora);
@@ -17,24 +18,19 @@ if (r == null) {
 }
 console.log(r);
 
-for (var i = 0; i < r.length; i++) {
+for (var i = 1; i < r.length; i++) {
     switch (i) {
         case 1:
             if (r[i] > 23) {
                 alert('La hora es incorrecta');
-                exit[0];
+                i  = r.length;
             }
             break; 
         case 2:
-            if (r[i] <= 60) {
-                alert('dd');
-            }
-            break;
         case 3:
             if (r[i] <= 60) {
                 alert('dd');
+                i  = r.length;
             }
-            break;
-    }
-   
+    }  
 }
