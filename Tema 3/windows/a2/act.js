863ea8aa4591110216607d15e040fc09de14dc03
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-10-23 18:03:17 +0200

    Actividad 2

--- Tema 3/windows/a2/act.js
+++ Tema 3/windows/a2/act.js
@@ -1,11 +1,18 @@
 window.onload = function() {
     var t = prompt('Introduce la cadena a buscar.');
-    window.find(t, false);
-    console.log(t);
-
-    while () {
-
+    var confir = true;
+    while(confir) {
+        var w = window.find(t, false);
+        confir = window.confirm('Deseas continuar buscando');
+        if (w == false) {
+            confir = false;
+            var r = window.confirm('Deseas salir');
+            if (r == true) {
+                window.closed;
+            } else {
+                this.location.reload();
+            }
+        }
     }
-
 }
 
