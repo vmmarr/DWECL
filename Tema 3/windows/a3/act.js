de2c2493d72fd4f0abe2d72f4599521f3ea0defb
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-10-23 21:11:10 +0200

    Actividad 3

--- Tema 3/windows/a3/act.js
+++ Tema 3/windows/a3/act.js
@@ -1,17 +1,19 @@
 window.onload = function() {
     var i = document.getElementById('ini');
     var p = document.getElementById('parar');
-    
-    
-    i.onclick = function() {
-        console.log('hola');
+    i.onclick = function(e) {
+        e.preventDefault();
         iniciar();
     }
-    p.onclick = function() {
+    p.onclick = function(e) {
+        e.preventDefault();
         clearInterval(m);
+        hora();
     }
 }
+
 var m;
+
 function hora() {
     var f = new Date();
     document.title = f.toLocaleTimeString();
