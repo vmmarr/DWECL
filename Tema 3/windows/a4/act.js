066e46405a7b17401e21e9bc1ae7ae5f3cc873b3
Maria del Mar <mariadelmar.vazquez@iesdonana.org>
2019-10-25 00:23:02 +0200

    Actividad 4 sin terminar con cambios

--- Tema 3/windows/a4/act.js
+++ Tema 3/windows/a4/act.js
@@ -1,10 +1,11 @@
-window.onload =function() {
-    setInterval(horaActual, 1000);
+window.onload =function(e) {
+    e.preventDefault();
     cboton();
+    setInterval(horaActual, 1000);
 }
+
 function horaActual() {
     var ha = new Date();
-
     var parrafos = document.getElementsByTagName('p');    
     
     if (parrafos.length == 0) {
@@ -19,6 +20,7 @@ function horaActual() {
         var nodo2 = document.createTextNode(ha.toLocaleTimeString());
         pf.replaceChild(nodo2, pf.firstChild);
     }
+    return ha;
 }
 
 function cboton() {
@@ -29,24 +31,40 @@ function cboton() {
     
     document.body.appendChild(b);
 
-    b.click(pedirHora());
+    b.addEventListener('click', pedirHora());
 }
-
 
 function pedirHora() {
     alert('dame');
 
     var p = prompt('A que hora te aviso hh:mm');
-        var patron = /^[\d{2}]:[\d{2}]$/;
-        var arrayPatron = patron.exec(p);
-
-        if (arrayPatron != null) {
-            if (arrayPatron[1] < 24 && arrayPatron[2] < 60) {
-                
-            }
-        } else {
-            alert('La hora no es correcta');
+    var patron = /^(\d{2}):(\d{2})$/;
+    var arrayPatron = patron.exec(p);
+    if (arrayPatron != null) {
+        if (arrayPatron[1] < 24 && arrayPatron[2] < 60) {
+            //return arrayPatron[1];
+            comprobar();
         }
+    } else {
+        alert('La hora no es correcta');
+    }
+}
 
-        // Comprobar la cadena
+function comprobar() {
+    var hActual = horaActual().getHours();
+    var mActual = horaActual().getMinutes();
+    var hNueva = arrayPatron[1];
+    var mNueva = arrayPatron[2];
+
+    if (hActual == hNueva && mActual == mNueva) {
+        prosponer();
+    }
+}
+
+function prosponer() {
+    var c = confirm('¿Dormir mas?');
+
+    if (c == true) {
+
+    }
 }
