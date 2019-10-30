dc83c098426b7256905e63facd1f9e47fae14633
Maria del Mar <mariadelmar.vazquez@iesdonana.org>
2019-10-30 16:04:13 +0100

    Actividad 1

--- Tema4/funciones-array/a1/act.js
+++ Tema4/funciones-array/a1/act.js
@@ -3,16 +3,23 @@ function convertir1() {
     var b1 = document.getElementById('base1').value;
     var r = document.getElementById('resultado1');
 
-    console.log(b1);
-    if (isNaN(n1)) {
-        n1.value = '';
-        alert('El numero no es correcto');
-    } else if (!isNaN(b1)) {
-        alert('La base no es correcta');
-        b1.value = '';
-    }
+    if (isNaN(n1) || isNaN(b1)) {
+        document.getElementById('num1').value = '';
+        document.getElementById('base1').value = '';
+        alert('El numero y la base no son correcto');
+    } 
 
+    n1 = parseInt(n1);
+    b1 = parseInt(b1);
     if (b1 >= 2 && b1 <= 36) {
         r.value = n1.toString(b1);
     }
+}
+
+function convertir2() {
+    var n2 = document.getElementById('num2').value;
+    var b2 = parseInt(document.getElementById('base2').value);
+    var r = document.getElementById('resultado2');
+
+    r.value = parseInt(n2, b2);
 }
