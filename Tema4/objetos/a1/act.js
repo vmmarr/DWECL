0fff4908f610058d11556015f9b19ee93dcca992
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-11-06 19:47:09 +0100

    Actividad 1

--- Tema4/objetos/a1/act.js
+++ Tema4/objetos/a1/act.js
@@ -5,22 +5,13 @@ function Vehiculo(marca, modelo, color, anio) {
     this.anio = anio;
 }
 
-function mostrarDatos(v) {
-    for (var i in v) {
-        if (v.hasOwnProperty(i)) {
-            console.log(`${i}: ${v[i]}`);	
-        }
-        
-    }
-}
-
-function acelerar(v, velocidad) {
-    console.log(`El coche ${v.marca} a acelerado ${velocidad} km`);
+Vehiculo.prototype.mostrarDatos = function() {
+    
 }
 
 var v1 = new Vehiculo('seat', 'ibiza', 'verde', '2009');
 var v2 = new Vehiculo('nissan', 'micra', 'azul', '2005');
 
-mostrarDatos(v1);
+v1.mostrarDatos();
 console.log(v1.marca);
 console.log(v2['color']);
