6e6f6c9f450a56fdf37754d550c90810b0c5ddc4
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-11-08 18:58:25 +0100

    Actividad 1 sin terminar

--- Tema4/objetos/a1/act.js
+++ Tema4/objetos/a1/act.js
@@ -3,15 +3,63 @@ function Vehiculo(marca, modelo, color, anio) {
     this.modelo = modelo;
     this.color = color;
     this.anio = anio;
+
+    this.arrancar = function() {
+        console.log(`El coche de marca ${this.marca}, modelo ${this.modelo}, color ${this.color} ha arrancado`);	
+    }
 }
 
 Vehiculo.prototype.mostrarDatos = function() {
-    
+    for (var e in this) {
+        if (this.hasOwnProperty(e)) {
+            console.log(`${e}: ${this[e]}`);	
+        }
+    }
+}
+
+Vehiculo.prototype.acelerar = function(velocidad) {
+    console.log(`El vehiculo ha acelerado ${velocidad} km`);
+}
+
+Vehiculo.prototype.cilindrada;
+
+Vehiculo.prototype.frenar = function() {
+    console.log(`El coche marca ${this.marca}, modelo ${this.modelo} y color ${this.color} ha parado`);
 }
 
 var v1 = new Vehiculo('seat', 'ibiza', 'verde', '2009');
 var v2 = new Vehiculo('nissan', 'micra', 'azul', '2005');
 
+Object.defineProperties(v1, {
+    marca: {
+        configurable: false
+    }, 
+    modelo: {
+        value: 'xx',
+        writable: false
+    },
+    arrancar: {
+        value: v1.arrancar,
+        enumerable: false
+    },
+    acelerar: {
+        value: v1.acelerar,
+        enumerable: false
+    },
+    frenar: {
+        value: v1.frenar,
+        enumerable: false
+    }
+});
+
+
 v1.mostrarDatos();
-console.log(v1.marca);
-console.log(v2['color']);
+v1.acelerar(120);
+v1.arrancar();
+v1.frenar();
+delete v1.marca;
+v1.modelo = 'c3';
+v1.mostrarDatos();
+console.log(Object.keys(v1));
+
+console.log(Object.getOwnPropertyNames(v1));
