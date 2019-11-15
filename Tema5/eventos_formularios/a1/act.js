33f74e7c3b73881a2c24ed16c567c85499ff6fbf
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-11-15 20:24:05 +0100

    Actividad 1 sin terminarç

---
+++ Tema5/eventos_formularios/a1/act.js
@@ -0,0 +1,19 @@
+var b = document.createElement('button');
+var t = document.createTextNode('Pulsame');
+b.appendChild(t);
+b.setAttribute('id', 'boton');
+document.body.appendChild(b);
+
+b.addEventListener('mousedown', pulsado);
+
+function pulsado() {
+    var o;
+    switch (o) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
