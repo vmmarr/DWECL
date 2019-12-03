1ed1d7c8f66c0621e93a9cb76b024f8d7df0dc7e
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2019-12-03 20:47:00 +0100

    Tiene error

--- Tema6/menu_desplegable/act.js
+++ Tema6/menu_desplegable/act.js
@@ -1,28 +1,62 @@
 var u = document.getElementById('lista');
 
 var a = [
-    ['Seccion uno', 's1'], 
+    ['Seccion uno', 's1', 'ss'], 
     ['Seccion dos'], 
     ['Seccion  tres', 's2']
 ];
 
 
 for (var i = 0; i < a.length; i++) {
-    var cabecera;
     for (var j = 0; j < a[i].length; j++) {
         if (j == 0) {
-            cabecera = document.createElement('li');
-            var n = document.createTextNode(a[i][j]);
-            cabecera.setAttribute('id', `seccion${i}`); 
-            cabecera.appendChild(n);
-            u.appendChild(cabecera); 
-        } else {
-            var nL = document.createElement('ul');
             var l = document.createElement('li');
             var n = document.createTextNode(a[i][j]);
+            l.setAttribute('id', `seccion${i+1}`); 
             l.appendChild(n);
-            nL.appendChild(l); 
-            cabecera.appendChild(nL);
+            u.appendChild(l); 
+        } else if (j == 1) {
+            var d = document.createElement('div');
+            d.setAttribute('id', `subseccion${i+1}`); 
+            var pa = document.createElement('p');
+            var en = document.createElement('a');
+            var n = document.createTextNode(a[i][j]);
+            d.appendChild(pa);
+            pa.appendChild(en);
+            en.appendChild(n);
+            l.appendChild(d);
+        } else {
+            var pa1 = document.createElement('p');
+            var en1 = document.createElement('a');
+            var n1= document.createTextNode(a[i][j]);
+            pa1.appendChild(en1);
+            en1.appendChild(n1);
+            d.appendChild(pa1);
         }
     }
+}
+
+
+var li = document.getElementsByTagName('li');
+console.log(li.length);
+
+for (var i = 0; i < li.length; i++) {
+    console.log(i);
+    li[i].addEventListener('mouseover', mostrar, false);
+    li[i].addEventListener('mouseout', ocultar, false);
+    // li[i].setAttribute('onmouseover', mostrar);
+    // li[i].setAttribute('onmouseout', ocultar);
+}
+
+function mostrar() {
+    n++;
+    var ss = document.getElementById("subseccion"+n).style.display="block";
+    console.log(ss);
+    
+    //document.getElementById("subseccion"+n).style.display="block";
+}
+function ocultar() {
+    n++;
+    console.log('oculta');
+    document.getElementById("subseccion"+n).style.display="none";
 }
