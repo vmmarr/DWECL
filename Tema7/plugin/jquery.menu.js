6445abb5a8a24410349da8b0e123f47c7f15f88e
Maria del Mar Vazquez Rodriguez <mariadelmar.iesdonana.org>
2020-01-27 19:45:28 +0100

    hago algunas modificaciones

--- Tema7/plugin/jquery.menu.js
+++ Tema7/plugin/jquery.menu.js
@@ -1,24 +1,43 @@
 (function($) {
     var metodos = {
         inicio: function() {
-            $('body').append('<ul></ul>');
-            $('ul').attr('id', 'lista');
+            $(this).append('<ul></ul>');
+            $(this).children([0]).attr('id', 'lista');
             return;
         },
     
-        addSection: function(texto) {
-            $('#lista').append(`<li>${texto}</li>`);
+        addSection: function(texto, posicion) {
+            var p = parseInt(posicion)+1;
+            // usar append y si esta ya usado que le pregunte si lo quiere modificar
+            if ($(this).children().length > 0) {
+                // $(`<li>${texto}</li>`).insertAfter(`li:eq(${p})`);
+                $(this).after(`<li>${texto}</li>`);
+            } else {
+                $(this).append(`<li>${texto}</li>`);
+            }
+
         },
 
         delSection: function() {
-            // Comprobar si la seccion tiene hijos probando con el metodo find() o coon el children()
+            // Comprobar si la seccion tiene hijos probando con el metodo find() o con el children()
             // amboos con el .length
-            $(this).remove();
+
+            // No  funciona bien
+            if ($(this).children().length > 0)  {
+                console.log('dhhd');
+                var c = confirm('¿Deseas borrar la seleccion y todos sus hijos?');
+                if (c == true) {
+                    $(this).remove();
+                }
+            } else {
+                $(this).remove();
+            }
         },
 
         addOption: function(texto) {
             if ($(this).children().length <= 0) {
                 $(this).append(`<ul></ul>`);
+                $(this).children([0]).attr('class', 'sub');
                 $(this).children([0]).append(`<li>${texto}</li>`);
             } else {
                 $(this).children([0]).append(`<li>${texto}</li>`);
@@ -28,6 +47,8 @@
         delOption: function() {
             // Comprobar si la seccion tiene hijos probando con el metodo find() o coon el children()
             // amboos con el .length
+
+            // No funciona bien
             $(this).remove();
         }
     };
--- Tema7/plugin/menu.js
+++ Tema7/plugin/menu.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
-    $().llamar('inicio');
-    $().llamar("addSection", "hola1");
-    $().llamar("addSection", "h2");
-    $().llamar("addSection", "h3");
-    $().llamar("addSection", "h4");
-    //$('li:eq(2)').llamar("delSection");
-    $('li:eq(2)').llamar('addOption', 'h3-1');
-    $('li:eq(2)').llamar('addOption', 'h3-2');
-    $('li:eq(2)').llamar('addOption', 'h3-3');
-    $('li:eq(2)').llamar('addOption', 'h3-4');
+    $('body').llamar('inicio');
 
-    $('li:eq(0)').llamar('addOption', 'h1-1');
-    $('li:eq(0)').llamar('addOption', 'h1-2');
-    $('li:eq(0)').llamar('addOption', 'h1-3');
-    $('li:eq(0)').llamar('addOption', 'h1-4');
+    $('body').append('<button>Añadir</button>').click(function (e) { 
+        e.preventDefault();
+        var texto = prompt('Introduce el nombre de la seccion');
+        var posicion = prompt('Introduce la posicion en la que deseas introducirla');
+        $('#lista').llamar('addSection', `${texto}`, `${posicion}`);
+        //$('#lista').llamar("addSection", `${texto}`);
+    });
+    
+    
+    // $('#lista').llamar("addSection", "h2");
+    // $('#lista').llamar("addSection", "h3");
+    // $('#lista').llamar("addSection", "h4");
+    // $('li:eq(2)').llamar('addOption', 'h3-1');
+    // $('li:eq(2)').llamar('addOption', 'h3-2');
+    // $('li:eq(2)').llamar('addOption', 'h3-3');
+    // $('li:eq(2)').llamar('addOption', 'h3-4');
+    
+    // $('li:eq(0)').llamar('addOption', 'h1-1');
+    // $('li:eq(0)').llamar('addOption', 'h1-2');
+    // $('li:eq(0)').llamar('addOption', 'h1-3');
+    // $('li:eq(0)').llamar('addOption', 'h1-4');
+    // //$('#lista').find('li:eq(0)').llamar("delSection");
+    // $('#lista').find().llamar("delSection");
 
-    // Solo me falta probar que me borre
-    // $('li:eq(5)').llamar('delOption');
+    // // Solo me falta probar que me borre
+    // // $('li:eq(5)').llamar('delOption');
 });
